4ecbbc22e28a966cd08456e40f836fc5be78a13d
moringspeaker <guchenyu150533@gmail.com>
2023-02-22 02:28:27 -0500

    1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
+import psycopg2
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,17 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%=g34odx*d8apw2w5x+y2^m$h+ax3^o+!#tbrc#)bd@vi@!7m&"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'django-polls-dev.us-west-2.elasticbeanstalk.com',  # make sure to replace it with your own EB CNAME
-    'localhost',
-    '127.0.0.1'
-]
+    'http://django-polls-dev3.us-west-2.elasticbeanstalk.com/',
+    'django-polls-dev3.us-west-2.elasticbeanstalk.com',
+    'localhost',]
 
 
 # Application definition
@@ -77,14 +74,30 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 }
-
+# if 'RDS_DB_NAME' in os.environ:
+#
+# else:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql_psycopg2",
+#             "NAME": "polls",
+#             "USER": "postgres",
+#             "PASSWORD": "gcy211139902",
+#             "HOST": "localhost",
+#             "PORT": "5432",
+#         }
+#
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
